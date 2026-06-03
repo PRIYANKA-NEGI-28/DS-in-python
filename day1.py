16d16926ad7d5826210143b6da7875f954ff695c
PRIYANKA-NEGI-28 <priyankaviveknegi@gmail.com>
2026-06-03 11:10:36 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,117 @@
+
+# print('hello world')
+
+# print('this machine')
+
+# a = 5
+# print(a)
+     
+
+# a=5
+# print(a+2*a+3*a+4*a+5*a)
+
+
+# a=input()
+# b=input()
+# c=input()
+# print(a+b+c)
+
+
+
+# a=int(input())
+# b=int(input())
+# c=int(input())
+# print(a+b+c)
+
+
+# a=int(input())
+# b=int(input())
+# c=int(input())
+# print(a+b+c)
+
+# if c is not defined it will take the value from previous code sometimes
+
+# Swap 2 numbers
+
+# a=4
+# b=5
+# print(a,b)
+# a=a+b
+# b=a-b
+# a=a-b
+# print(a,b)
+
+# colon is uswd after loop statement like for , if, else, else if and remember to use indentation
+
+# for i in range (1,51):
+#   if i%2==0:
+#      print(i, 'even')
+#   else:
+#       print(i, 'odd')
+
+
+
+# a=int(input())
+# if a>0:
+#   print('positive')
+# elif a<0:
+#   print('negative')
+# else:
+#   print('zero')
+     
+
+# assignment operator and comparison operator with string literals like a ='mon'
+
+# a=input()
+# if a == 'sun':
+#   print('sunday')
+# elif a == 'mon':
+#   print('monday')
+# elif a== 'tue':
+#   print('tuesday')
+# elif a== 'wed':
+#    print('wednesday')
+# elif a=='thurs':
+#    print('thursday')
+# elif a=='fri':
+#     print('friday')
+# elif a=='sat':
+#   print('saturday')
+
+
+
+#   for i in range(10):
+#   print(i)
+     
+# for i in range(start, end, step) in for loop range the start s included and end is excluded
+
+
+# for i in range(0,10,2):
+#   print(i)
+
+#   a++ a-- these operators are not in python, assign the value like a=a+1 to inc or a=a-1 to dec
+
+
+
+# a=1
+
+# while a<10:
+#   print(a)
+#   a=a+1
+
+
+
+# / -> float divison gives float values on divison 
+# //-> floor divison gives int values on divison 
+# *-> multiplication 
+# **-> power
+
+
+
+# # palindrome no or not 
+# n = input("enter string: ")
+
+# if n == n[::-1]:
+#     print("Palindrome")
+# else:
+#     print("Not Palindrome")
